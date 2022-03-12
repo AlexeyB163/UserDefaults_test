b58baa39db775b78715a20d09d79687f3f67a51b
AlexeyB163 <sourceaisa@mail.ru>
2022-03-12 19:50:16 +0400

    Initial Commit

---
+++ UserDefaults_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaults_test
+//
+//  Created by User on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
